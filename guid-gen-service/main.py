45026799b949699fbf61948a5d20375f393a9b88
Luke Church <lukechurch@google.com>
2014-09-29 13:05:32 +0100

    Add CORS header

--- guid-gen-service/main.py
+++ guid-gen-service/main.py
@@ -3,6 +3,7 @@ import uuid
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+        self.response.headers.add_header("Access-Control-Allow-Origin", "*")
         self.response.write(str(uuid.uuid4()))
 
 app = webapp2.WSGIApplication([
